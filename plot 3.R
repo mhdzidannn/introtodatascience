11d4f4aa65b048006486baaa2b852f8d784acb4f
Zaidan <41482733+mhdzidannn@users.noreply.github.com>
2019-06-21 16:46:53 +0800

    Update plot 3.R

--- plot 3.R
+++ plot 3.R
@@ -1,27 +1,32 @@
 ### PLOT 3 ###
+#set working directory
+setwd("C:/Users/Salimah/Documents/data/exdata/")
 
 library(data.table)
 
-## Getting the entire dataset : Electric power consumption
-data_entire <- household_power_consumption
+## Getting full dataset
+DT<-fread("./household_power_consumption.txt", na.strings="?")
 
-## Subsetting the data from the dates 2007-02-01 and 2007-02-02
-data_entire$Date <- as.Date(data_entire$Date, format="%d/%m/%Y")
-data <- subset(data_entire, subset=(Date >= "2007-02-01" & Date <= "2007-02-02"))
-rm(data_entire)
+DT$Date <- as.Date(DT$Date, format="%d/%m/%Y")
+
+## Subsetting the data
+DT.filter <- subset(DT, subset=(Date >= "2007-02-01" & Date <= "2007-02-02"))
+rm(DT)
 
 ## Converting dates
-datetime <- paste(as.Date(data$Date), data$Time)
-data$Datetime <- as.POSIXct(datetime)
+datetime <- paste(as.Date(DT.filter$Date), DT.filter$Time)
+DT.filter$Datetime <- as.POSIXct(datetime)
 
-## Plotting and save in a file named plot3.png
-png(filename="plot3.png", width=480, height=480)
-with(data, {
+#plot the graph with legends
+with(DT.filter, {
   plot(Sub_metering_1~Datetime, type="l",
        ylab="Energy sub metering", xlab="")
-  lines(Sub_metering_2 ~ Datetime,col='Red')
-  lines(Sub_metering_3 ~ Datetime,col='Blue')}
-)
-legend("topright", col=c("black", "red", "blue"), lty=1, lwd=2, 
-       legend=c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"))
+  lines(Sub_metering_2~Datetime,col='Red')
+  lines(Sub_metering_3~Datetime,col='Blue')
+})
+legend("topright", col=c("black", "red", "blue"), lty=1, lwd=2,
+       legend=c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"), cex = 0.75)
+
+## output histogram to PNG
+dev.copy(png, file="plot3.png", height=480, width=480)
 dev.off()
