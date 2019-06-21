850b7d1d2cc8169ae29989744cf3688ed44ccf90
Zaidan <41482733+mhdzidannn@users.noreply.github.com>
2019-06-21 16:49:35 +0800

    Update plot4.R

--- plot4.R
+++ plot4.R
@@ -1,36 +1,38 @@
 ### PLOT 4 ###
+#set working directory
+setwd("C:/Users/Salimah/Documents/data/exdata/")
 
 library(data.table)
 
-## Getting the entire dataset : Electric power consumption
-data_entire <- household_power_consumption
-  
-## Subsetting the data from the dates 2007-02-01 and 2007-02-02
-data_entire$Date <- as.Date(data_entire$Date, format="%d/%m/%Y")
-data <- subset(data_entire, subset=(Date >= "2007-02-01" & Date <= "2007-02-02"))
-rm(data_entire)
+## Getting full dataset
+DT<-fread("./household_power_consumption.txt", na.strings="?")
+
+DT$Date <- as.Date(DT$Date, format="%d/%m/%Y")
+
+## Subsetting the data
+DT.filter <- subset(DT, subset=(Date >= "2007-02-01" & Date <= "2007-02-02"))
+rm(DT)
 
 ## Converting dates
-datetime <- paste(as.Date(data$Date), data$Time)
-data$Datetime <- as.POSIXct(datetime)
+datetime <- paste(as.Date(DT.filter$Date), DT.filter$Time)
+DT.filter$Datetime <- as.POSIXct(datetime)
 
-#Output plot 4 to png
-png(filename="plot4.png", width=480, height=480)
-
+#plot the graph with legends
 par(mfrow=c(2,2), mar=c(4,4,2,1), oma=c(0,0,2,0))
-with(data, {
-  plot(Global_active_power ~ Datetime, type="l", 
+with(DT.filter, {
+  plot(Global_active_power~Datetime, type="l",
        ylab="Global Active Power", xlab="")
-  plot(Voltage ~ Datetime, type="l", 
-       ylab="Voltage", xlab="datetime")
-  plot(Sub_metering_1 ~ Datetime, type="l", 
-       ylab="Energy Sub Meteringr", xlab="")
-  lines(Sub_metering_2 ~ Datetime, col='Red')
-  lines(Sub_metering_3 ~ Datetime, col='Blue')
-  legend("topright", col=c("black", "red", "blue"), lty=1, lwd=2, bty="n",
-         legend=c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"))
-  plot(Global_reactive_power ~ Datetime, type="l", 
-       ylab="Global_reactive_power",xlab="datetime") }
-)
+  plot(Voltage~Datetime, type="l",
+       ylab="Voltage (volt)", xlab="datetime")
+  plot(Sub_metering_1~Datetime, type="l",
+       ylab="Energy sub metering", xlab="")
+  lines(Sub_metering_2~Datetime,col='Red')
+  lines(Sub_metering_3~Datetime,col='Blue')
+  legend("topright", col=c("black", "red", "blue"), lty=1, lwd=2, bty="n", legend=c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"), cex = 0.75)
+  plot(Global_reactive_power~Datetime, type="l",
+       ylab="Global_Rective_Power",xlab="datetime")
+})
+## Saving to file
+dev.copy(png, file="plot4.png", height=480, width=480)
 dev.off()
 
